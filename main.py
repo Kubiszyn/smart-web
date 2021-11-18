1b848a2cc73f3eefe8a43093c8fec3d71bf6b192
Dawid 2ndPC <kubiszyndawid101@gmail.com>
2021-11-18 13:13:52 +0100

    Created new page for contact, added some text, new pictures and new route in python file.

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = 'secret'
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def main():
     return render_template('home.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    return render_template('contact.html')
 
 app.run(host='127.0.0.1', port=8000, debug=True)
