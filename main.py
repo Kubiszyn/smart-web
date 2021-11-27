8f04f4e7f24da9a3dcfddde1d021af52468e9f88
Dawid <11029@student.eadania.dk>
2021-11-27 13:45:40 +0100

    Added a package calculator and few tweaks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 
 app = Flask(__name__)
 app.secret_key = 'secret'
@@ -17,7 +17,31 @@ def service():
 
 @app.route('/package', methods=['GET', 'POST'])
 def package():
-    return render_template('package.html')
+    msg = 'None'
+    if request.method == 'POST' and 'kilograms' in request.form:
+        msg = 'none'
+        kg = int(request.form['kilograms'])
+        if kg < int(1): 
+            msg = '15.90 PLN'
+            return render_template('package.html', msg=msg)
+        elif kg >= int(1) and kg < int(5): 
+            msg = '19.90'
+            return render_template('package.html', msg=msg)
+        elif kg >= int(5) and kg < int(15) : 
+            msg = '20.90'
+            return render_template('package.html', msg=msg)
+        elif kg >= int(15) and kg < int(31) : 
+            msg = '22.90'
+            return render_template('package.html', msg=msg)
+        elif kg == int(31): 
+            msg = '24.90 '
+            return render_template('package.html', msg=msg)
+        elif kg > int(31):
+            overprice  = kg - 31
+            msg = (overprice * 1.50) + 24.90
+            return render_template('package.html', msg=msg)
+    else:
+        return render_template('package.html', msg=msg)
 
 @app.route('/buysell', methods=['GET', 'POST'])
 def buysell():
